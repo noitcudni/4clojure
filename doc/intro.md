96f50927ab50f483cfedbc1ee052c492932f8657
Lih Chen <lihster@gmail.com>
2014-11-10 11:14:08 -0800

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-problem-set
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_problem_set/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-problem-set.core-test
+  (:require [clojure.test :refer :all]
+            [clj-problem-set.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
